11ece2fd89ef8e13bb1f339e24b82a3d4ba8cbf8
Luddinus <luddinus@gmail.com>
2017-08-05 01:26:08 +0200

    Set correct status code
    Fixed https://github.com/esbenp/heimdal/issues/9

--- src/Formatters/UnprocessableEntityHttpExceptionFormatter.php
+++ src/Formatters/UnprocessableEntityHttpExceptionFormatter.php
@@ -10,6 +10,8 @@ class UnprocessableEntityHttpExceptionFormatter extends BaseFormatter
 {
     public function format(JsonResponse $response, Exception $e, array $reporterResponses)
     {
+        $response->setStatusCode(422);
+        
         // Laravel validation errors will return JSON string
         $decoded = json_decode($e->getMessage(), true);
         // Message was not valid JSON
